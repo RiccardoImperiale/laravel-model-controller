b28cfefb8417cc44e58e0c5110b8aa6b54192bf8
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-09 14:08:14 +0200

    add forelse loop to get all movies info

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Movies List</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -16,8 +16,20 @@
 </head>
 
 <body>
-
-    @dd($movies)
+    <h1>Movies</h1>
+    <div class="container">
+        @forelse ($movies as $movie)
+            <div class="movie">
+                <h2>{{ $movie->title }}</h2>
+                <h4>{{ $movie->original_title }}</h4>
+                <h6>{{ $movie->nationality }}</h6>
+                <h6>{{ $movie->date }}</h6>
+                <h6>{{ $movie->vote }}</h6>
+            </div>
+        @empty
+            <p>Sorry, no movies to be found...</p>
+        @endforelse
+    </div>
 
 </body>
 
